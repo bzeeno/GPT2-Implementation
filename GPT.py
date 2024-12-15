af9182fad0c69fb2c2f3f9022a4093bcec325591
cpzeeno@gmail.com <cpzeeno@gmail.com>
2024-12-14 20:04:48 -0800

    Create skeleton for GPTModel. Add initial definitions of LayerNorm and TransformerLayer.

--- GPT.py
+++ GPT.py
@@ -1,2 +1,23 @@
 import torch
 import torch.nn as nn
+
+from gpt2_config import GPT2_CONFIG_124M
+from Transformer import TransformerLayer
+
+class LayerNorm(nn.Module):
+    def __init__():
+        super().__init__()
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        # Inputs to transformer layers
+        self.token_embeddings = nn.Embedding(cfg["vocab_size"], cfg["embedding_dimension"])
+        self.pos_embeddings = nn.Embedding(cfg["context_length"], cfg["embedding_dimension"])
+
+        # Transformer layers
+        self.transformer_layers = nn.Sequential(*[TransformerLayer(cfg) for _ in range(cfg["n_layers"])])
+        
+        # Output layers
+        self.final_layer_norm = LayerNorm(cfg["embedding_dimension"])
+        self.out_head = nn.Linear(cfg["embedding_dimension"], cfg["vocab_size"])
---
+++ Transformer.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+
+class TransformerLayer(nn.Module):
+    def __init__():
+        super().__init__()
