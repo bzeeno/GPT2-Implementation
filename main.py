8898ef9bc989ea9eef8e34f017084015fc128461
cpzeeno@gmail.com <cpzeeno@gmail.com>
2024-12-24 21:04:33 -0800

    Create methods for getting the number of parameters of the model and for getting the memory requirements for the model.
    Create requirements.txt file for dependencies.

--- main.py
+++ main.py
@@ -17,6 +17,24 @@ def gen_text_sample(model, token_seq, max_new_tokens, context_length):
         token_seq = torch.cat((token_seq, next_tok), dim=1)
     return token_seq
 
+def encode(text, tokenizer):
+    encoded = tokenizer.encode(text)
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
+def decode(token_ids, tokenizer):
+    flat = token_ids.squeeze(0) # remove batch dimension
+    return tokenizer.decode(flat.tolist())
+
+def get_num_paramters(model):
+    total_params = sum(p.numel() for p in model.parameters())
+    return total_params
+
+def get_mem_requirements(total_params):
+    total_size_bytes = total_params * 4
+    total_size_mb = total_size_bytes / (1024 * 1024)
+    return total_size_mb
+
 def main():
     torch.manual_seed(24)
     model = GPTModel(GPT2_CONFIG_124M)
@@ -25,17 +43,15 @@ def main():
 
     # Tokenize Input
     tokenizer = tiktoken.get_encoding("gpt2")
-    encoded = tokenizer.encode(starting_context)
-    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
-
-    # Print num of parameters and memory requirements
-    total_params = sum(p.numel() for p in model.parameters())
-    print(f"Total number of parameters: {total_params:,}")
-    total_size_bytes = total_params * 4
-    total_size_mb = total_size_bytes / (1024 * 1024)
-    print(f"Total size of the model: {total_size_mb:.2f} MB")
+    encoded_tensor = encode(starting_context, tokenizer)
 
     print("input:", encoded_tensor)
+
+    # Print num of parameters and memory requirements
+    total_params = print_num_paramters(model)
+    print(f"Total number of parameters: {total_params:,}")
+    total_size_mb = get_mem_requirements(total_params)
+    print(f"Total size of the model: {total_size_mb:.2f} MB")
 
     # Run inference on model
     out = gen_text_sample(
@@ -47,7 +63,6 @@ def main():
     print("Output:", out)
     print("Output length:", len(out[0]))
     # Decode outputs
-    decoded_text = tokenizer.decode(out.squeeze(0).tolist())
-    print(decoded_text)
+    print(decode(out, tokenizer))
 
 main()
